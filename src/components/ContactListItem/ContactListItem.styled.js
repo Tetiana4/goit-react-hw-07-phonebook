720b856a9644ac9d507b881a521e2b7d8ec4c420
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2021-10-16 20:26:44 +0200

    Add styles

---
+++ src/components/ContactListItem/ContactListItem.styled.js
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+export const ListItem = styled.li`
+  display: flex;
+  text-align: center;
+  justify-content: space-between;
+`;
+
+export const Button = styled.button`
+  width: 70px;
+  margin-bottom: 5px;
+  padding: 5px 10px;
+  color: #f71c0c;
+  border: 1px solid #723a3a;
+  border-radius: 5px;
+`;
