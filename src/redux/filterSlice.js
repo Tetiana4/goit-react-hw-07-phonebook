fb5c9b7d82ff209519c2fab503a7ff2a0744f880
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2021-10-18 14:36:51 +0200

    Delete files

--- src/redux/filterSlice.js
+++
@@ -1,8 +0,0 @@
-import * as actions from './actions';
-import { createReducer } from '@reduxjs/toolkit';
-
-export const filter = createReducer('', {
-  [actions.filterContact]: (_state, { payload }) => {
-    return payload;
-  },
-});
